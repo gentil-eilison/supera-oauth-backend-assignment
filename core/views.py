aeb11f6148bcf93ca99339ddfab26cbecc41c949
Matheus Bento <matheus51bento@gmail.com>
2024-08-07 10:35:28 -0300

    fix: view

--- core/views.py
+++ core/views.py
@@ -1,5 +1,4 @@
 from dj_rest_auth.registration.views import SocialLoginView
-
 from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
@@ -21,7 +20,7 @@ class GitHubLogin(SocialLoginView):
 
 class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
-    callback_url = "http://127.0.0.1:8000"
+    callback_url = "http://localhost:3000/login"
     client_class = OAuth2Client
 
 def decode_code_view(request):
